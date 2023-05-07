01a505ceff4371b66c74667a2f3677f572c7b6ea
praveen <pravgaddms@gmail.com>
2023-05-07 19:04:50 +0530

    gave worker version

--- manual_eks_vpc/eks/worker.tf
+++ manual_eks_vpc/eks/worker.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_node_group" "backend" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "dev"
+  version         = aws_eks_cluster.eks.version
   node_role_arn   = aws_iam_role.eks_worker.arn
   subnet_ids      = [var.subnet_ids[0], var.subnet_ids[1]]
   capacity_type   = "SPOT"
